8fcfc994c16743d45be696237559fccfd4c4bbea
NapasornTilapornputt <napasorn.til@dome.tu.ac.th>
2023-11-11 11:17:37 +0700

    Add sum array reduce

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 function sum(a,b) {
     if (Array.isArray(a)) {
-        let sum = 0
-        a.forEach((i) => (sum += i))
-        return sum
-    } else if (typeof a === 'number' && typeof b === 'number') {
-        return a + b
-    }
+             return a.reduce((acc,n) => acc + n, 0)
+         } else if (typeof a === 'number' && typeof b === 'number') {
+             return a + b
+         }
+    // if (Array.isArray(a)) {
+    //     let sum = 0
+    //     a.forEach((i) => (sum += i))
+    //     return sum
+    // } else if (typeof a === 'number' && typeof b === 'number') {
+    //     return a + b
+    // }
     // if (Array.isArray(a)) {
     //     let total = 0
     //     for (let i = 0; i < a.length; i++) {
